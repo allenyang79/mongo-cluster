f15e4a38b21d5eb2237429f92707a4c2208b51d9
Allen Yang <allenyang@appier.com>
2019-03-05 18:30:03 +0800

    docker-compose

--- 1_init-config-server.js
+++ 1_init-config-server.js
@@ -8,7 +8,7 @@ rs.initiate(
     members: [
       { _id : 0, host : "config-server-1:27019" },
       { _id : 1, host : "config-server-2:27019" },
-      { _id : 2, host : "config-server-3:27019" }
+      { _id : 2, host : "config-server-3:27019", priority:0}
     ]
   }
 )
